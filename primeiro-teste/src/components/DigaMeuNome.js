a67209d6c381a4fc8eaf7876d4f8ebc2587f1839
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2025-09-03 16:25:42 -0300

    Delete primeiro-teste directory

--- primeiro-teste/src/components/DigaMeuNome.js
+++
@@ -1,8 +0,0 @@
-function DigaMeuNome(props){
-    return(
-        <div>
-            <p>Olá {props.nome}, Seja bem vindo!</p>
-        </div>
-    )
-}
-export default DigaMeuNome
--- primeiro-teste/src/components/HelloWord.js
+++
@@ -1,8 +0,0 @@
-function HelloWord(){
-    return(
-        <div>
-            <h1>MEU PRIMEIRO COMPONETE</h1>
-        </div>
-    )
-}
-export default HelloWord
